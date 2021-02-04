088e4f75d1921de8661815ef36e3bf2043566186
Lucimara Rios Nakata <lurnakata@gmail.com>
2021-02-03 22:40:50 -0300

    add cors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(bodyParser.json());
 app.use(routes);
 
